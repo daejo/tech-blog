f7c81d1b890ea9224527b6895c39fed0497c3fd9
daejo <daejo@icloud.com>
2020-10-22 21:50:09 -0700

    fixes controller files

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ const express = require('express'); // links express into file
 const app = express(); // links express into app
 const PORT = process.env.PORT || 3001; // specifies and links local port into file
 
-// ======== CONNECTION REQUIREMENTS ======== //
+// ======== ROUTE REQUIREMENTS ======== //
+const routes = require('./controllers');
 
+
+// turn on routes
+app.use(routes);
 
 // turns on connection to server
 sequelize.sync({ force: false }).then(() => {
