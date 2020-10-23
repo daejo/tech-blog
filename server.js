355b50afeb7ea2f6c3ed49c884c55a4c43b46702
daejo <daejo@icloud.com>
2020-10-22 21:32:49 -0700

    pushes new files

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 // npm init -y  ||  initialize npm
 // npm i mysql2 sequelize express bcrypt dotenv  ||  installed dependencies
+
+// ======== CONNECTION REQUIREMENTS ======== //
 const sequelize = require('./config/connection'); // links sequelize into file
 const express = require('express'); // links express into file
 const app = express(); // links express into app
 const PORT = process.env.PORT || 3001; // specifies and links local port into file
+
+// ======== CONNECTION REQUIREMENTS ======== //
 
 
 // turns on connection to server
